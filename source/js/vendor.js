6dd7c9226059de2eaae9abeeb493ab2daeb9ecc5
Cashkacaca <Cashkacaca@gmail.com>
2023-05-22 23:56:37 +0300

    Делаю табы

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,4 +1,12 @@
 // Swiper 7.4.1
 import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
+// Табы
+import {initTabs} from './vendor/init-tabs';
 
+
+window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('load', () => {
+    initTabs();
+  });
+});
